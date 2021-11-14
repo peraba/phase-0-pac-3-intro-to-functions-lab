84db300b85d685c0dfb7f4ede3169a7f52a9eaaa
Pedro Ramos <medpsr9@gmail.com>
2021-11-14 16:36:42 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+const string = "sayHiToGrandma";
+console.log(string);
+console.log(string.toUpperCase());
+
+string === string.toUpperCase;
+
+console.log(string);
+console.log(string.toLowerCase());
+string === string.toLowerCase;
